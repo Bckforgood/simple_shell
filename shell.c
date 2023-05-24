dbf2a40a3dca6ae99e50aa6487a9aa7b081409fb
Bckforgood <t.abdo.khbaza@gmail.com>
2023-05-24 10:06:07 -0700

    small test

--- shell.c
+++ shell.c
@@ -1,60 +1,29 @@
-#include <unistd.h>
-#include <string.h>
-#include <stdlib.h>
 #include "shell.h"
-#define BUFFER_SIZE 1024
+/**
+ * shell - executes a cmd in a new pross
+ * @argv: command and arguments
+ *
+ * Return: always return 0
+ */
+int shell(char **argv)
+{
+	pid_t pid;
+	int stats;
 
-void display_prompt() {
-    char prompt[] = "#cisfun$ ";
-    write(STDOUT_FILENO, prompt, sizeof(prompt) - 1);
+	pid = fork();
+
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		execve(argv[0], argv, environ);
+		perror("Error:");
+		exit(EXIT_FAILURE);
+	}
+	else
+		waitpid(pid, &stats, 0);
+	return (0);
 }
-
-int execute_command(char *line) {
-    pid_t pid;
-    
-    pid = fork();
-    
-    if (pid == -1) {
-        perror("fork");
-        return -1;
-    }
-    
-    if (pid == 0) {
-        char **args = malloc(sizeof(char *) * 2);
-        args[0] = line;
-        args[1] = NULL;
-        
-        execve(line, args, NULL);
-        perror("execve");
-        exit(1);
-    } else {
-        int status;
-        wait(&status);
-    }
-    
-    return 0;
-}
-
-int main() {
-    char line[BUFFER_SIZE];
-    int status = 0;
-    
-    while (1) {
-        display_prompt();
-        
-        if (fgets(line, sizeof(line), stdin) == NULL) {
-            char goodbye[] = "Goodbye!\n";
-            write(STDOUT_FILENO, goodbye, sizeof(goodbye) - 1);
-            break;
-        }
-        
-        line[strcspn(line, "\n")] = '\0';
-        
-        if (execute_command(line) == -1) {
-            break;
-        }
-    }
-    
-    return status;
-}
-
