fcf030d2bc152b85c01f43dbfa4f44eb52a1ae54
Bckforgood <t.abdo.khbaza@gmail.com>
2023-05-24 10:14:59 -0700

    shell.c

--- shell.c
+++ shell.c
@@ -1,29 +1,89 @@
 #include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+extern char **environ;
+
 /**
- * shell - executes a cmd in a new pross
- * @argv: command and arguments
+ * main - Entry point of the shell
  *
- * Return: always return 0
+ * Return: Always 0
  */
-int shell(char **argv)
+int main(void)
 {
-	pid_t pid;
-	int stats;
+	char *line = NULL;
+	size_t len = 0;
+	ssize_t input;
+	char *prompt = "#cisfun$ ";
+	char **argv;
 
-	pid = fork();
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+			write(STDOUT_FILENO, prompt, strlen(prompt));
 
-	if (pid == -1)
-	{
-		perror("fork");
-		exit(EXIT_FAILURE);
+		input = getline(&line, &len, stdin);
+		if (input == -1)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 1);
+			free(line);
+			break;
+		}
+
+		if (input == 1 && line[0] == '\n')
+			continue;
+
+		line[input - 1] = '\0'; /* Remove the newline character */
+
+		argv = malloc(sizeof(char *) * 2);
+		if (argv == NULL)
+		{
+			perror("Allocation error");
+			exit(EXIT_FAILURE);
+		}
+
+		argv[0] = line;
+		argv[1] = NULL;
+
+		if (strcmp("exit", argv[0]) == 0)
+		{
+			free(line);
+			free(argv);
+			break;
+		}
+
+		if (access(argv[0], F_OK) == 0)
+		{
+			pid_t pid = fork();
+
+			if (pid == -1)
+			{
+				perror("fork");
+				exit(EXIT_FAILURE);
+			}
+			else if (pid == 0)
+			{
+				execve(argv[0], argv, environ);
+				perror("Error");
+				exit(EXIT_FAILURE);
+			}
+			else
+			{
+				waitpid(pid, NULL, 0);
+			}
+		}
+		else
+		{
+			printf("%s: command not found\n", argv[0]);
+		}
+
+		free(argv);
 	}
-	else if (pid == 0)
-	{
-		execve(argv[0], argv, environ);
-		perror("Error:");
-		exit(EXIT_FAILURE);
-	}
-	else
-		waitpid(pid, &stats, 0);
+
 	return (0);
 }
+
