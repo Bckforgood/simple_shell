9252442ea87fbc362799573b29ee7a27d58e6bc3
Bckforgood <t.abdo.khbaza@gmail.com>
2023-05-24 15:17:42 -0700

    fixxing exit func

--- shell.c
+++ shell.c
@@ -28,3 +28,38 @@ int shell(char **argv)
 		waitpid(pid, &status, 0);
 	return (0);
 }
+/**
+  * exitShell - fun exit shell with status
+  * @argv: args passed in by user
+  * @line: line
+  * Return: 0 or exit status
+  */
+int exitShell(char *argv[], char *line)
+{
+    int exitStatus = 0;
+    int i = 0;
+
+    if (argv[1] == NULL || (!_strcmp(argv[1], "0")))
+    {
+        free(line);
+        exit(0);
+    }
+    else
+    {
+        while (argv[1][i] != '\0')
+        {
+            if (argv[1][i] < '0' || argv[1][i] > '9')
+            {
+                char error_message[50] = "hsh: exit: ";
+                strcat(error_message, argv[1]);
+                strcat(error_message, ": numeric argument required\n");
+                write(STDERR_FILENO, error_message, strlen(error_message));
+                free(line);
+                exit(2);
+            }
+            exitStatus = exitStatus * 10 + (argv[1][i] - '0');
+            i++;
+        }
+    }
+    return (exitStatus);
+}
