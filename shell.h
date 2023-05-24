3f0d753e3bd1166c1a6cb1ad244ec45a11d4b4e9
Alexus91 <mrfadlkamal@gmail.com>
2023-05-24 07:15:17 -0700

    shell.c fixing

---
+++ shell.h
@@ -0,0 +1,21 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+
+extern char **environ;
+
+int _putchar(char c);
+int _strcmp(char *s1, char *s2);
+int shell(char **argv);
+int Path(char **env, char **argv);
+char *_strcpy(char *d, char *s);
+char *_strcat(char *d, char *s);
+char *_strdup(char *s);
+char *_memcpy(char *d, char *s, unsigned int n);
+#endif
